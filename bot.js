b2f86c63268e7271c6b7f3ad924b879c649b1b0d
Rodrigo <rrivem@gmail.com>
2018-03-21 16:59:38 -0300

    add hint for bot message

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const itsTimeMessage = (user, minsToGo) =>
-	`Hola ${user.name}, te aviso que te tocan los masajes en ${minsToGo} minutos.`;
+	`Hola ${user.name}, te aviso que te tocan los masajes en ${minsToGo} minutos. (Este mensaje fue enviado por un bot)`;
 
 class BotService {
 	itsTime(user, minsToGo) {
