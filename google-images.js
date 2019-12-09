164d2f774582ac447a1fd3c5eb0ef14e11a27268
Rodrigo Rivera <rrivem@gmail.com>
2019-12-09 09:16:37 -0300

    ensure we get an image

--- google-images.js
+++ google-images.js
@@ -3,15 +3,21 @@ const config = require('./config').googleImages;
 
 class GoogleImagesService {
 	async getRandomImage(query) {
-		const start = Math.floor(Math.random() * 10);
-		const url = 'https://www.googleapis.com/customsearch/v1?cx=[engineKey]&key=[apiKey]&q=[query]&searchType=image&safe=high&start=[start]'
-			.replace('[engineKey]', config.engineKey)
-			.replace('[apiKey]', config.apiKey)
-			.replace('[query]', query)
-			.replace('[start]', start);
+		let imageUrl;
+		while (!imageUrl) {
+			const start = Math.floor(Math.random() * 10);
+			const url = 'https://www.googleapis.com/customsearch/v1?cx=[engineKey]&key=[apiKey]&q=[query]&searchType=image&safe=high&start=[start]'
+				.replace('[engineKey]', config.engineKey)
+				.replace('[apiKey]', config.apiKey)
+				.replace('[query]', query)
+				.replace('[start]', start);
 
-		const { items } = await fetch(url).then(res => res.json());
-		return items[Math.floor(Math.random() * items.length)].link;
+			const { items } = await fetch(url).then(res => res.json());
+			if (items) {
+				imageUrl = items[Math.floor(Math.random() * items.length)].link;
+			}
+		}
+		return imageUrl;
 	}
 }
 
